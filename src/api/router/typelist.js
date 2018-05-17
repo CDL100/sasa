19837d0f58435d208afdb553d3661bb40337897b
kangyin <m13434135160@163.com>
2018-05-17 15:02:40 +0800

    lk

--- src/api/router/typelist.js
+++ src/api/router/typelist.js
@@ -6,8 +6,15 @@ module.exports = {
     getType(app){
         app.get('/type',async (req,res)=>{
             let type_id = req.query.data;
-            console.log(type_id)
             result = await db.select('goodstype',{type_id});
+            res.send(result);
+        })
+    },
+    getList(app){
+        app.get('/datalist',async (req,res)=>{
+            let type_id = req.query.type;
+            console.log(type_id)
+            result = await db.select('goods',{type_id});
             res.send(result);
         })
     }
--- src/components/Type/TypeList/TypeList.js
+++ src/components/Type/TypeList/TypeList.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import http from '../../../utils/HttpClient.js'
+import {Router, Route, hashHistory, Link, browserHistory} from 'react-router'
 //Element type is invalid：expected a string or a class/function...没有写export default
 export default class TypeList extends React.Component{
     state={
         config:{
             url: 'type',
-            data: '13',
+            data: '13'
         },
-        dataset:[]
+        dataset:[],
+        data:'13'
     }
     componentDidMount(){
         let {url, data, method='get'} = this.state.config;
@@ -18,24 +20,29 @@ export default class TypeList extends React.Component{
         })
     }
     changeType(type){
-        this.setState({config:{url:'type',data:type}});
-        let {url, data, method='get'} = this.state.config;
-        http[method](url, {data}).then((res) => {
-            this.setState({dataset:res.data});
-        }).catch((error) => {
-           console.log(error)
+        this.setState({config:{url:'type',data:type}},()=>{
+            let {url, data, method='get'} = this.state.config;
+            http[method](url, {data}).then((res) => {
+                this.setState({dataset:res.data});
+            }).catch((error) => {
+               console.log(error)
+            })
         })
     }
     render(){
         let list;
+        let type;
         if(this.state.dataset[0]){
+            type = this.state.dataset[0].type_id;
             list = (
-                this.state.dataset[0].category_column.map(item=>{
+                this.state.dataset[0].category_column.map((item,idx)=>{
                     return(
-                        <li key={item.type_id}>
-                            <span>{item.name}</span>
-                            <i className="fa fa-arrow-right"></i>
-                        </li>
+                        <Link to={'/goodslist/'+type} key={idx}>
+                            <li >
+                                <span>{item.name}</span>
+                                <i className="fa fa-arrow-right"></i>
+                            </li>
+                        </Link>
                     )
                 })
             )
